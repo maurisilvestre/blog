b77f1cc001d3ec5e6c02c6851cd5e10ca88ec9d4
Mauri Couto <mauri.sciensa@genial.com.vc>
2022-05-31 16:27:04 -0300

    fix: fixes to small resolutions

--- lib/features/auth/presentation/pages/sign_in_page.dart
+++ lib/features/auth/presentation/pages/sign_in_page.dart
@@ -33,99 +33,101 @@ class SignInPage extends StatelessWidget {
                   padding: const EdgeInsets.all(8.0),
                   child: Form(
                     key: _formKey,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(
-                          "Microblogging",
-                          style: themeData.textTheme.headline3!.copyWith(
-                            color: themeData.primaryColorLight,
-                            fontWeight: FontWeight.bold,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            "Microblogging",
+                            style: themeData.textTheme.headline3!.copyWith(
+                              color: themeData.primaryColorLight,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
-                        const SizedBox(
-                          height: 40,
-                        ),
-                        Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            TextFormField(
-                              controller: _emailController,
-                              autovalidateMode:
-                                  AutovalidateMode.onUserInteraction,
-                              validator: (value) {
-                                return Validations().fieldRequired(value);
-                              },
-                              autofocus: true,
-                              autocorrect: false,
-                              decoration: const InputDecoration(
-                                hintText: 'E-mail',
+                          const SizedBox(
+                            height: 40,
+                          ),
+                          Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              TextFormField(
+                                controller: _emailController,
+                                autovalidateMode:
+                                    AutovalidateMode.onUserInteraction,
+                                validator: (value) {
+                                  return Validations().fieldRequired(value);
+                                },
+                                autofocus: true,
+                                autocorrect: false,
+                                decoration: const InputDecoration(
+                                  hintText: 'E-mail',
+                                ),
                               ),
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            TextFormField(
-                              obscureText: true,
-                              controller: _passwordController,
-                              autovalidateMode:
-                                  AutovalidateMode.onUserInteraction,
-                              validator: (value) {
-                                return Validations().fieldRequired(value);
-                              },
-                              autocorrect: false,
-                              decoration: const InputDecoration(
-                                hintText: 'Senha',
+                              const SizedBox(
+                                height: 10,
                               ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 40),
-                        SizedBox(
-                          width: MediaQuery.of(context).size.height * 0.3,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              if (_formKey.currentState!.validate()) {
-                                _formKey.currentState?.save();
-
-                                BlocProvider.of<AuthNotifier>(context).add(
-                                  AuthEvent.signIn(
-                                    SignIn(
-                                      email: _emailController.text,
-                                      password: _passwordController.text,
+                              TextFormField(
+                                obscureText: true,
+                                controller: _passwordController,
+                                autovalidateMode:
+                                    AutovalidateMode.onUserInteraction,
+                                validator: (value) {
+                                  return Validations().fieldRequired(value);
+                                },
+                                autocorrect: false,
+                                decoration: const InputDecoration(
+                                  hintText: 'Senha',
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 40),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.height * 0.3,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  _formKey.currentState?.save();
+                    
+                                  BlocProvider.of<AuthNotifier>(context).add(
+                                    AuthEvent.signIn(
+                                      SignIn(
+                                        email: _emailController.text,
+                                        password: _passwordController.text,
+                                      ),
                                     ),
+                                  );
+                                }
+                                // Navigator.push(
+                                //   context,
+                                //   MaterialPageRoute(
+                                //       builder: (context) => const HomePage()),
+                                // );
+                              },
+                              child: const Text("Entrar"),
+                            ),
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.height * 0.3,
+                            child: OutlinedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  PageRouteBuilder(
+                                    pageBuilder: (c, a1, a2) => SignUpPage(),
+                                    transitionsBuilder: (c, anim, a2, child) =>
+                                        FadeTransition(
+                                            opacity: anim, child: child),
+                                    transitionDuration:
+                                        const Duration(milliseconds: 500),
                                   ),
                                 );
-                              }
-                              // Navigator.push(
-                              //   context,
-                              //   MaterialPageRoute(
-                              //       builder: (context) => const HomePage()),
-                              // );
-                            },
-                            child: const Text("Entrar"),
+                              },
+                              child: const Text("Registrar"),
+                            ),
                           ),
-                        ),
-                        SizedBox(
-                          width: MediaQuery.of(context).size.height * 0.3,
-                          child: OutlinedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                PageRouteBuilder(
-                                  pageBuilder: (c, a1, a2) => SignUpPage(),
-                                  transitionsBuilder: (c, anim, a2, child) =>
-                                      FadeTransition(
-                                          opacity: anim, child: child),
-                                  transitionDuration:
-                                      const Duration(milliseconds: 500),
-                                ),
-                              );
-                            },
-                            child: const Text("Registrar"),
-                          ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),
